49434b9e6767d86fd6eaa9888782abf0756b5eaf
Mayur Kotian <mayurkotian16@gmail.com>
2021-03-04 21:12:16 -0500

    added print line

--- WEB-API/01-Requests/00-requests.py
+++ WEB-API/01-Requests/00-requests.py
@@ -5,9 +5,10 @@ def main():
     response = requests.get("https://api.exchangeratesapi.io/latest", params = payload)
     if response.status_code != 200:
         print("Status code: ", response.status_code)
-        raise Exception("There was an error! ")
+        raise Exception("There was a majour error! ")
     data = response.json()
     print("JSON data", data)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
